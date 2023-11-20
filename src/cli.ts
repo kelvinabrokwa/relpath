63bc974d6b8ec2c5583d91cd47667f849c450e16
Alistair Hutten <ahut10@gmail.com>
2023-11-20 16:16:13 +1100

    feature copy to clipboard

--- src/cli.ts
+++ src/cli.ts
@@ -3,9 +3,12 @@
 
 import { exit } from 'process';
 import { relativePath } from './index'
+import { copy as copyClipboard } from 'copy-paste';
 
 const args = process.argv.slice(2);
 const paths: string[] = [];
+
+let copy = false;
 
 function displayHelp(exitCode = 1) {
     console.log(`
@@ -13,9 +16,11 @@ Usage: relpath [options] <from_file> <to_file>\n
 
 Options:
   -h, --help       Display this help message
+  -c, --copy       Copy the result to the clipboard
 
 Examples:
   relative-path-tool /path/to/first/file /path/to/second/file
+  relative-path-tool -c /path/to/first/file /path/to/second/file
     `);
     exit(exitCode);
 }
@@ -23,6 +28,9 @@ Examples:
 
 for (let arg of args) {
     switch (true) {
+        case arg === '-c' || arg === '--copy':
+            copy = true;
+            break;
         case arg === '-h' || arg === '--help':
             displayHelp(1);
             break;
@@ -41,3 +49,8 @@ const [fromPath, toPath] = paths;
 const result = relativePath(fromPath, toPath);
 
 console.log(result);
+
+if (copy) {
+    copyClipboard(result);
+    console.log(`have written to clipboard for you`);
+}
