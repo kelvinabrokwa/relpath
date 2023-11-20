1ceb7526b47ef5fc9d0fa7348da0216d7b8b1b0a
Alistair Hutten <ahut10@gmail.com>
2023-11-20 16:06:44 +1100

    feature-reverse-order

--- src/cli.ts
+++ src/cli.ts
@@ -7,15 +7,19 @@ import { relativePath } from './index'
 const args = process.argv.slice(2);
 const paths: string[] = [];
 
+let reverse = false, copy = false;
+
 function displayHelp(exitCode = 1) {
     console.log(`
-Usage: relpath [options] <from_file> <to_file>\n
-
-Options:
-  -h, --help       Display this help message
+    Usage: relpath [options] <from_file> <to_file>\n
+    
+    Options:
+    -h, --help       Display this help message
+    -r, --reverse    Reverse the order of the paths
 
 Examples:
   relative-path-tool /path/to/first/file /path/to/second/file
+  relative-path-tool -r /path/to/first/file /path/to/second/file
     `);
     exit(exitCode);
 }
@@ -23,6 +27,9 @@ Examples:
 
 for (let arg of args) {
     switch (true) {
+        case arg === '-r' || arg === '--reverse':
+            reverse = true;
+            break;
         case arg === '-h' || arg === '--help':
             displayHelp(1);
             break;
@@ -36,6 +43,8 @@ if (paths.length !== 2) {
     console.error('You must provide exactly two paths.');
     displayHelp(1);
 }
+
+if (reverse) paths.reverse();
 
 const [fromPath, toPath] = paths;
 const result = relativePath(fromPath, toPath);
